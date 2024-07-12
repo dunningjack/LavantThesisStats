42260d9a70f9ee670e5ebed4e28c941010e1d9a1
dunningjack <67054303+dunningjack@users.noreply.github.com>
2024-07-12 20:44:05 +0100

    Establish source file for required packages to reduce clutter + save time

---
+++ Init.R
@@ -0,0 +1,38 @@
+install.packages("tidyverse")
+install.packages("vegan")
+install.packages("ggplot2")
+install.packages("ggfortify")
+install.packages("devtools")
+devtools::install_github("gavinsimpson/ggvegan")
+install.packages("car")
+
+
+library("tidyverse")
+library("vegan")
+library("ggplot2")
+library("ggfortify")
+library("ggvegan")
+library("car")
+
+
+data<-read.csv("Lavant Data .csv", header = TRUE)
+#read in csv file, assign to data
+
+class(data)
+#display class of data
+
+summary(data)
+#display summary of data
+
+data$Site<-as.factor(data$Site)
+data$Site<-factor(data$Site, levels=c("US1", "US2", "DS1", "DS2"), ordered=TRUE)
+#convert Site column (character) to factor, set levels of factor
+groups<-c("US1", "US2", "DS1", "DS2")
+
+testVars <- c("ASPT", "Temperature", "pH", "DO", "TDS", "Nitrate", "Phosphate", "Faecal.Coliforms", "Escherichia.coli", "Enterococci") #, "Shannon", "Simpson"
+for (var in testVars){
+  data[[var]] <- as.numeric(data[[var]])
+}
+#store all columns as numeric
+
+sites <- unique(data$Site)
--- screening.R
+++ screening.R
@@ -1,28 +1,4 @@
-source("env.R")
-
-data<-read.csv("Lavant Data .csv", header = TRUE)
-#read in csv file, assign to data
-
-class(data)
-#display class of data
-
-summary(data)
-#display summary of data
-
-data$Site<-as.factor(data$Site)
-data$Site<-factor(data$Site, levels=c("US1", "US2", "DS1", "DS2"), ordered=TRUE)
-#convert Site column (character) to factor, set levels of factor
-groups<-c("US1", "US2", "DS1", "DS2")
-
-testVars <- c("ASPT", "Temperature", "pH", "DO", "TDS", "Nitrate", "Phosphate", "Faecal.Coliforms", "Escherichia.coli", "Enterococci") #, "Shannon", "Simpson"
-for (var in testVars){
-  data[[var]] <- as.numeric(data[[var]])
-}
-#store all columns as numeric
-
-sites <- unique(data$Site)
-
-
+source("Init.R")
 
 for (var in testVars) {
   print(
