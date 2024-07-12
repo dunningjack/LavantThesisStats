8786d7dcc498416b7fe3bfc78009059a66647ccd
dunningjack <67054303+dunningjack@users.noreply.github.com>
2024-07-12 20:01:42 +0100

    Init
    screening.R complete

--- screening.R
+++ screening.R
@@ -26,7 +26,7 @@ data$Site<-factor(data$Site, levels=c("US1", "US2", "DS1", "DS2"), ordered=TRUE)
 #convert Site column (character) to factor, set levels of factor
 groups<-c("US1", "US2", "DS1", "DS2")
 
-testVars <- c("BMWP", "ASPT", "Temperature", "pH", "DO", "TDS", "Nitrate", "Phosphate") #, "Shannon", "Simpson"
+testVars <- c("ASPT", "Temperature", "pH", "DO", "TDS", "Nitrate", "Phosphate", "Faecal.Coliforms", "Escherichia.coli", "Enterococci") #, "Shannon", "Simpson"
 for (var in testVars){
   data[[var]] <- as.numeric(data[[var]])
 }
@@ -50,15 +50,54 @@ for (var in testVars) {
     test_result <- shapiro.test(data[[var]])
 
     if (test_result$p.value <0.05) {
-      cat("\n Shapiro-Wilk Test for Normality for ", var, " at site \n")
+      cat("\n Shapiro-Wilk Test for Normality for ", var," \n")
       print(test_result)
       print("Reject the null hypothesis, data is not normally distributed")
+      print("============================================================================")
     } else {
       cat("\n Shapiro-Wilk Test for Normality for ", var,"\n")
       print(test_result)
       print("Fail to reject the null hypothesis, data is normally distributed")
+      print("============================================================================")
       }
     }
   }
-#test normality and variance
-#qq plots and boxplots
+
+
+for (var in testVars){
+  qqnorm(data[[var]], main=paste("Q-Q plot of", var))
+    qqline(data[[var]])
+}
+#qq plots w/ line
+
+
+library("car")
+results <- list()
+levene_pf <- list()
+for (var in testVars) {
+  if (is.numeric(data[[var]])) {
+    testFormula <- as.formula(paste(var, "~ Site"))
+    leveneResult <- leveneTest(testFormula, data = data)
+    results[[var]] <- leveneResult
+    levene_pf[[var]] <- leveneResult$`Pr(>F)`[1]
+  } else {
+    cat("\nSkipping", var, "as it is not a numeric variable.\n")
+  }
+}
+#test homogeneity of variance w/ levenme
+
+
+for (var in names(results)){
+  cat("\n Levene's Test for Homogeneity of Variance for ", var, "\n")
+  print(results[[var]])
+  if (levene_pf[[var]] < 0.05) {
+    print("Reject the null hypothesis, variances are not homogeneous")
+  } else {
+    print("Fail to reject the null hypothesis, variances are homogeneous")
+  }
+  print("============================================================================")
+}
+#print each result for levene
+
+testVars <- c("Distance.From.Outflow", "ASPT", "Temperature", "pH", "DO", "TDS", "Nitrate", "Phosphate", "Faecal.Coliforms", "Escherichia.coli", "Enterococci") #, "Shannon", "Simpson"
+#include distance as not needed to test for normality etc
