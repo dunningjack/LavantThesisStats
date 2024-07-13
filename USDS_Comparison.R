3ffa8b165ed9500d27cf68410c28dcc4b88a78f8
dunningjack <67054303+dunningjack@users.noreply.github.com>
2024-07-13 13:19:39 +0100

    Upstream/Downstream Comparisons for each variable completed by t-test
    TODO: Query whether t-test is most appropriate test for scenario

--- USDS_Comparison.R
+++ USDS_Comparison.R
@@ -5,3 +5,20 @@
 data <- data %>%
   mutate(SiteGroup = if_else(str_detect(Site, "^DS"), "DS", "US"))
 #mark each site as up or downstream
+
+USDS_Differences <- data.frame(Variable=character(), Difference=character(), p.value=numeric())
+
+for (var in testVars){
+  cat("T-test for", var, "between US and DS\n")
+  ttest_result <- t.test(data[[var]] ~ data$SiteGroup)
+
+  if (ttest_result$p.value < 0.05) {
+    cat("Reject the null hypothesis, there is a significant difference between US and DS\n")
+    USDS_Differences <- rbind(USDS_Differences, data.frame(Variable=var, Difference="Significant", p.value=ttest_result$p.value))
+  } else {
+    cat("Fail to reject the null hypothesis, there is no significant difference between US and DS\n")
+    USDS_Differences <- rbind(USDS_Differences, data.frame(Variable=var, Difference="Not Significant", p.value=ttest_result$p.value))
+  }
+}
+cat("\nSummary of differences between US and DS\n")
+print(USDS_Differences)
