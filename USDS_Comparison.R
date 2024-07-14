af448d5205d322459d87aff646ddf350f3f73f30
dunningjack <67054303+dunningjack@users.noreply.github.com>
2024-07-14 20:33:01 +0100

    Addition of microbial comparison as marker for discharges
    TODO: Query whether t-test is most appropriate test for scenario

--- USDS_Comparison.R
+++ USDS_Comparison.R
@@ -9,15 +9,12 @@ data <- data %>%
 USDS_Differences <- data.frame(Variable=character(), Difference=character(), t.value=character(), p.value=numeric())
 
 for (var in testVars){
-  cat("T-test for", var, "between US and DS\n")
   ttest_result <- t.test(data[[var]] ~ data$SiteGroup, var.equal=TRUE)
   #independent samples, variances homogeneous as tested previously
 
   if (ttest_result$p.value < 0.05) {
-    cat("Reject the null hypothesis, there is a significant difference between US and DS\n")
     USDS_Differences <- rbind(USDS_Differences, data.frame(Variable=paste0(var, "*"), Difference="Significant", t.value=ttest_result$statistic, p.value=ttest_result$p.value))
   } else {
-    cat("Fail to reject the null hypothesis, there is no significant difference between US and DS\n")
     USDS_Differences <- rbind(USDS_Differences, data.frame(Variable=var, Difference="Not Significant", t.value=ttest_result$statistic, p.value=ttest_result$p.value))
   }
 }
--- microbialCor.R
+++ microbialCor.R
@@ -1,26 +1,66 @@
 cor_res <- cor.test(data$Nitrate, data$Faecal.Coliforms, method = "pearson")
 if (cor_res$p.value < 0.05) {
   cat("Reject the null hypothesis, there is a significant correlation between Faecal Coliforms and Nitrate\n")
-  cat("Correlation coefficient:", cor_res$estimate, "\n")
+  cat("Correlation coefficient:", cor_res$estimate, "t-value:", cor_res$statistic, "p-value:", cor_res$p.value, "\n")
+  print("============================================================================ \n \n")
 } else {
   cat("Fail to reject the null hypothesis, there is no significant correlation between Faecal Coliforms and Nitrate\n")
+    print("============================================================================ \n \n")
 }
 #test correlation between nitrate and faecal coliforms
 
 cor_res <- cor.test(data$Nitrate, data$Escherichia.coli, method = "pearson")
 if (cor_res$p.value < 0.05) {
   cat("Reject the null hypothesis, there is a significant correlation between Escherichia coli and Nitrate\n")
-  cat("Correlation coefficient:", cor_res$estimate, "\n")
+  cat("Correlation coefficient:", cor_res$estimate, "t-value:", cor_res$statistic, "p-value:", cor_res$p.value, "\n")
+  print("============================================================================ \n \n")
 } else {
   cat("Fail to reject the null hypothesis, there is no significant correlation between Escherichia coli and Nitrate\n")
+    print("============================================================================ \n \n")
 }
 #test correlation between nitrate and escherichia coli
 
 cor_res <- cor.test(data$Nitrate, data$Enterococci, method = "pearson")
 if (cor_res$p.value < 0.05) {
   cat("Reject the null hypothesis, there is a significant correlation between Enterococci and Nitrate\n")
-  cat("Correlation coefficient:", cor_res$estimate, "\n")
+  cat("Correlation coefficient:", cor_res$estimate, "t-value:", cor_res$statistic, "p-value:", cor_res$p.value, "\n")
+    print("============================================================================ \n \n")
 } else {
   cat("Fail to reject the null hypothesis, there is no significant correlation between Enterococci and Nitrate\n")
+    print("============================================================================ \n \n")
 }
 #test correlation between nitrate and enterococci
+
+
+cor_res <- cor.test(data$Phosphate, data$Faecal.Coliforms, method = "pearson")
+if (cor_res$p.value < 0.05) {
+  cat("Reject the null hypothesis, there is a significant correlation between Faecal Coliforms and Phosphate\n")
+  cat("Correlation coefficient:", cor_res$estimate, "t-value:", cor_res$statistic, "p-value:", cor_res$p.value, "\n")
+    print("============================================================================ \n \n")
+} else {
+  cat("Fail to reject the null hypothesis, there is no significant correlation between Faecal Coliforms and Phosphate\n")
+    print("============================================================================ \n \n")
+}
+#test correlation between phosphate and faecal coliforms
+
+cor_res <- cor.test(data$Phosphate, data$Escherichia.coli, method = "pearson")
+if (cor_res$p.value < 0.05) {
+  cat("Reject the null hypothesis, there is a significant correlation between Escherichia coli and Phosphate\n")
+  cat("Correlation coefficient:", cor_res$estimate, "t-value:", cor_res$statistic, "p-value:", cor_res$p.value, "\n")
+  print("============================================================================ \n \n")
+} else {
+  cat("Fail to reject the null hypothesis, there is no significant correlation between Escherichia coli and Phosphate\n")
+  print("============================================================================ \n \n")
+}
+#test correlation between phosphate and escherichia coli
+
+cor_res <- cor.test(data$Phosphate, data$Enterococci, method = "pearson")
+if (cor_res$p.value < 0.05) {
+  cat("Reject the null hypothesis, there is a significant correlation between Enterococci and Phosphate\n")
+  cat("Correlation coefficient:", cor_res$estimate, "t-value:", cor_res$statistic, "p-value:", cor_res$p.value, "\n")
+    print("============================================================================ \n \n")
+} else {
+  cat("Fail to reject the null hypothesis, there is no significant correlation between Enterococci and Phosphate\n")
+    print("============================================================================ \n \n")
+}
+#test correlation between phosphate and enterococci
