b0d3a257c30218619bc7b512716dc3a48ec3b7c1
dunningjack <67054303+dunningjack@users.noreply.github.com>
2024-07-14 22:30:57 +0100

    Improved logging of results
    TODO: Query whether t-test is most appropriate test for scenario

--- microbialCor.R
+++ microbialCor.R
@@ -1,66 +1,53 @@
+microbialPearson <- data.frame(Variable=character(), Correlation=character(), Significant=character(), t.value=character(), p.value=numeric())
+
 cor_res <- cor.test(data$Nitrate, data$Faecal.Coliforms, method = "pearson")
+
 if (cor_res$p.value < 0.05) {
-  cat("Reject the null hypothesis, there is a significant correlation between Faecal Coliforms and Nitrate\n")
-  cat("Correlation coefficient:", cor_res$estimate, "t-value:", cor_res$statistic, "p-value:", cor_res$p.value, "\n")
-  print("============================================================================ \n \n")
+  microbialPearson <- rbind(microbialPearson, data.frame(Variable="Nitrate vs Faecal Coliforms", Correlation="Significant", Significant="***", t.value=cor_res$statistic, p.value=cor_res$p.value))
 } else {
-  cat("Fail to reject the null hypothesis, there is no significant correlation between Faecal Coliforms and Nitrate\n")
-    print("============================================================================ \n \n")
+    microbialPearson <- rbind(microbialPearson, data.frame(Variable="Nitrate vs Faecal Coliforms", Correlation="Not Significant", Significant="", t.value=cor_res$statistic, p.value=cor_res$p.value))
 }
 #test correlation between nitrate and faecal coliforms
 
 cor_res <- cor.test(data$Nitrate, data$Escherichia.coli, method = "pearson")
 if (cor_res$p.value < 0.05) {
-  cat("Reject the null hypothesis, there is a significant correlation between Escherichia coli and Nitrate\n")
-  cat("Correlation coefficient:", cor_res$estimate, "t-value:", cor_res$statistic, "p-value:", cor_res$p.value, "\n")
-  print("============================================================================ \n \n")
+  microbialPearson <- rbind(microbialPearson, data.frame(Variable="Nitrate vs Escherichia coli", Correlation="Significant", Significant="***", t.value=cor_res$statistic, p.value=cor_res$p.value))
 } else {
-  cat("Fail to reject the null hypothesis, there is no significant correlation between Escherichia coli and Nitrate\n")
-    print("============================================================================ \n \n")
+    microbialPearson <- rbind(microbialPearson, data.frame(Variable="Nitrate vs Escherichia coli", Correlation="Not Significant", Significant="", t.value=cor_res$statistic, p.value=cor_res$p.value))
 }
 #test correlation between nitrate and escherichia coli
 
 cor_res <- cor.test(data$Nitrate, data$Enterococci, method = "pearson")
 if (cor_res$p.value < 0.05) {
-  cat("Reject the null hypothesis, there is a significant correlation between Enterococci and Nitrate\n")
-  cat("Correlation coefficient:", cor_res$estimate, "t-value:", cor_res$statistic, "p-value:", cor_res$p.value, "\n")
-    print("============================================================================ \n \n")
-} else {
-  cat("Fail to reject the null hypothesis, there is no significant correlation between Enterococci and Nitrate\n")
-    print("============================================================================ \n \n")
+    microbialPearson <- rbind(microbialPearson, data.frame(Variable="Nitrate vs Enterococci", Correlation="Significant", Significant="***", t.value=cor_res$statistic, p.value=cor_res$p.value))
+    } else {
+        microbialPearson <- rbind(microbialPearson, data.frame(Variable="Nitrate vs Enterococci", Correlation="Not Significant", Significant="", t.value=cor_res$statistic, p.value=cor_res$p.value))
 }
 #test correlation between nitrate and enterococci
 
 
 cor_res <- cor.test(data$Phosphate, data$Faecal.Coliforms, method = "pearson")
 if (cor_res$p.value < 0.05) {
-  cat("Reject the null hypothesis, there is a significant correlation between Faecal Coliforms and Phosphate\n")
-  cat("Correlation coefficient:", cor_res$estimate, "t-value:", cor_res$statistic, "p-value:", cor_res$p.value, "\n")
-    print("============================================================================ \n \n")
-} else {
-  cat("Fail to reject the null hypothesis, there is no significant correlation between Faecal Coliforms and Phosphate\n")
-    print("============================================================================ \n \n")
+    microbialPearson <- rbind(microbialPearson, data.frame(Variable="Phosphate vs Faecal Coliforms", Correlation="Significant", Significant="***", t.value=cor_res$statistic, p.value=cor_res$p.value))
+    } else {
+        microbialPearson <- rbind(microbialPearson, data.frame(Variable="Phosphate vs Faecal Coliforms", Correlation="Not Significant", Significant="", t.value=cor_res$statistic, p.value=cor_res$p.value))
 }
 #test correlation between phosphate and faecal coliforms
 
 cor_res <- cor.test(data$Phosphate, data$Escherichia.coli, method = "pearson")
 if (cor_res$p.value < 0.05) {
-  cat("Reject the null hypothesis, there is a significant correlation between Escherichia coli and Phosphate\n")
-  cat("Correlation coefficient:", cor_res$estimate, "t-value:", cor_res$statistic, "p-value:", cor_res$p.value, "\n")
-  print("============================================================================ \n \n")
-} else {
-  cat("Fail to reject the null hypothesis, there is no significant correlation between Escherichia coli and Phosphate\n")
-  print("============================================================================ \n \n")
+    microbialPearson <- rbind(microbialPearson, data.frame(Variable="Phosphate vs Escherichia coli", Correlation="Significant", Significant="***", t.value=cor_res$statistic, p.value=cor_res$p.value))
+    } else {
+        microbialPearson <- rbind(microbialPearson, data.frame(Variable="Phosphate vs Escherichia coli", Correlation="Not Significant", Significant="", t.value=cor_res$statistic, p.value=cor_res$p.value))
 }
 #test correlation between phosphate and escherichia coli
 
 cor_res <- cor.test(data$Phosphate, data$Enterococci, method = "pearson")
 if (cor_res$p.value < 0.05) {
-  cat("Reject the null hypothesis, there is a significant correlation between Enterococci and Phosphate\n")
-  cat("Correlation coefficient:", cor_res$estimate, "t-value:", cor_res$statistic, "p-value:", cor_res$p.value, "\n")
-    print("============================================================================ \n \n")
-} else {
-  cat("Fail to reject the null hypothesis, there is no significant correlation between Enterococci and Phosphate\n")
-    print("============================================================================ \n \n")
+    microbialPearson <- rbind(microbialPearson, data.frame(Variable="Phosphate vs Enterococci", Correlation="Significant", Significant="***", t.value=cor_res$statistic, p.value=cor_res$p.value))
+    } else {
+        microbialPearson <- rbind(microbialPearson, data.frame(Variable="Phosphate vs Enterococci", Correlation="Not Significant", Significant="", t.value=cor_res$statistic, p.value=cor_res$p.value))
 }
 #test correlation between phosphate and enterococci
+
+print(microbialPearson)
