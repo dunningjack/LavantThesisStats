e6848b1bf90ffadbcecf664b5b4f60d9c8e13196
dunningjack <67054303+dunningjack@users.noreply.github.com>
2024-07-15 15:51:04 +0100

    Specifc CRAN mirror for each session

--- CompleteAnalysis.R
+++ CompleteAnalysis.R
@@ -1,7 +1,9 @@
+options(repos = c(CRAN = "https://cran.ma.imperial.ac.uk/"))
+
 source("Init.R")
 source("screening.R")
 source("dataTransforms.R")
 #source("imputeValues.R")
 source("USDS_Comparison.R")
 source("microbialCor.R")
-#source("AllSites_physicochemical.R")
+source("AllSites_physicochemical.R")
