239c0600b2d9e94b8ab046543b6f1ffd45b87e48
dunningjack <67054303+dunningjack@users.noreply.github.com>
2024-07-15 15:43:15 +0100

    Physicochemical analysis - ANOVA and Tukey HSD complete
    TODO: Query whether t-test is most appropriate test for scenario

--- AllSites_physicochemical.R
+++ AllSites_physicochemical.R
@@ -1,2 +1,25 @@
 #do param vary by site
 #aspt impacted by ??
+
+physdata <- data.frame(Site=character(), Variable=character(), F=character(), p=character())
+
+manova_res <- manova(cbind(Temperature_transformed, DO_transformed, TDS_transformed) ~ Site, data = data_transformed)
+#determine if difference between sites for physicochemical variables
+
+anovaTables <- list()
+tukeyResults <- list()
+
+testVars <- c("Temperature_transformed", "DO_transformed", "TDS_transformed")
+for (var in testVars) {
+  # Perform ANOVA for each variable separately
+  formula <- as.formula(paste(var, "~ Site"))
+  anova_res <- aov(formula, data = data_transformed)
+
+  anovaTables[[var]] <- summary(anova_res)
+  tukeyResults[[var]] <- TukeyHSD(anova_res)
+  #tukey pairwise
+
+}
+
+# Print Tukey's HSD results
+print(tukeyResults)
